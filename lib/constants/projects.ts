5c11beab4bbd1d4941a59b8ae2a0b35536861950
sirezo <joebertcerezo@gmail.com>
2025-07-09 16:13:49 +0800

    Edit projects vue

---
+++ lib/constants/projects.ts
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "title 1",
+    description: "A productivity app with real-time collaboration, drag-and-drop functionality, and team management.",
+    stack: ["1", "2", "3", "4"],
+    demo: "demo 1",
+    code: "code 1"
+  },
+  {
+    title: "title 2",
+    description: "description 2",
+    stack: ["1", "2", "3", "4"],
+    demo: "demo 2",
+    code: "code 2"
+  },
+  {
+    title: "title 3",
+    description: "description 3",
+    stack: ["1", "2", "3", "4"],
+    demo: "demo 3",
+    code: "code 3"
+  },
+  {
+    title: "title 3",
+    description: "description 3",
+    stack: ["1", "2", "3", "4"],
+    demo: "demo 3",
+    code: "code 3"
+  },
+  {
+    title: "title 3",
+    description: "description 3",
+    stack: ["1", "2", "3", "4"],
+    demo: "demo 3",
+    code: "code 3"
+  }
+]
